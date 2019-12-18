7b2b98a0f17116738911d8d9b1c9405bab4e54e9
mirzamanib <mirzamanib@gmail.com>
2019-12-19 00:48:10 +0330

    webpack bundle success

--- app/javascript/packs/pages-bundle.js
+++ app/javascript/packs/pages-bundle.js
@@ -1,7 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import Landing from '../bundles/pages/componentns/Landing';
+import Landing from '../bundles/pages/Landing';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
