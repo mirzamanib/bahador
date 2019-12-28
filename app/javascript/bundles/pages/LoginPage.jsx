468d056719749056bd743b33f617d556aa1ffa4b
Behrang <behrang@Behrangs-iMac.local>
2019-12-28 16:03:24 +0330

    WIP resolving rendering theme

--- app/javascript/bundles/pages/LoginPage.jsx
+++ app/javascript/bundles/pages/LoginPage.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
+// // @material-ui/icons
 import Email from "@material-ui/icons/Email";
 import People from "@material-ui/icons/People";
-// core components
+// // core components
 import Header from "../../components/Header/Header.js";
 import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import Footer from "../../components/Footer/Footer.js";
+import Footer from "../../components/Footer/Footer";
 import GridContainer from "../../components/Grid/GridContainer.js";
 import GridItem from "../../components/Grid/GridItem.js";
 import Button from "../../components/CustomButtons/Button.js";
@@ -25,132 +25,133 @@ import image from "../../assets/img/bg7.jpg";
 
 const useStyles = makeStyles(styles);
 
-console.log('*******************')
+console.log('*******************', styles.pageHeader, '\n', useStyles);
 
 export default function LoginPage(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function() {
-    setCardAnimation("");
-  }, 700);
-  const classes = useStyles();
+  // const [cardAnimation, setCardAnimation] = React.useState("cardHidden");
+  // setTimeout(function() {
+  //   setCardAnimation("");
+  // }, 700);
+  // const classes = useStyles();
+  // console.log('pageHeader', classes);
   const { ...rest } = props;
   return (
-    <div>
-      <Header
-        absolute
-        color="transparent"
-        brand="Material Kit React"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url(" + image + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        <div className={classes.container}>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                <form className={classes.form}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                    <h4>Login</h4>
-                    <div className={classes.socialLine}>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-twitter"} />
+      <div>
+        <Header
+            absolute
+            color="transparent"
+            brand="Material Kit React"
+            rightLinks={<HeaderLinks />}
+            {...rest}
+        />
+        <div
+            className={styles.pageHeader}
+            style={{
+              backgroundImage: "url(" + image + ")",
+              backgroundSize: "cover",
+              backgroundPosition: "top center"
+            }}
+        >
+          <div className={styles.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={4}>
+                <Card>
+                  <form className={styles.form}>
+                    <CardHeader color="primary" className={styles.cardHeader}>
+                      <h4>Login</h4>
+                      <div className={styles.socialLine}>
+                        <Button
+                            justIcon
+                            href="#pablo"
+                            target="_blank"
+                            color="transparent"
+                            onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-twitter"} />
+                        </Button>
+                        <Button
+                            justIcon
+                            href="#pablo"
+                            target="_blank"
+                            color="transparent"
+                            onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-facebook"} />
+                        </Button>
+                        <Button
+                            justIcon
+                            href="#pablo"
+                            target="_blank"
+                            color="transparent"
+                            onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-google-plus-g"} />
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <p className={styles.divider}>Or Be Classical</p>
+                    <CardBody>
+                      <CustomInput
+                          labelText="First Name..."
+                          id="first"
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            type: "text",
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                  <People className={styles.inputIconsColor} />
+                                </InputAdornment>
+                            )
+                          }}
+                      />
+                      <CustomInput
+                          labelText="Email..."
+                          id="email"
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            type: "email",
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                  <Email className={styles.inputIconsColor} />
+                                </InputAdornment>
+                            )
+                          }}
+                      />
+                      <CustomInput
+                          labelText="Password"
+                          id="pass"
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            type: "password",
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                  <Icon className={styles.inputIconsColor}>
+                                    lock_outline
+                                  </Icon>
+                                </InputAdornment>
+                            ),
+                            autoComplete: "off"
+                          }}
+                      />
+                    </CardBody>
+                    <CardFooter className={styles.cardFooter}>
+                      <Button simple color="primary" size="lg">
+                        Get started
                       </Button>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-facebook"} />
-                      </Button>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-google-plus-g"} />
-                      </Button>
-                    </div>
-                  </CardHeader>
-                  <p className={classes.divider}>Or Be Classical</p>
-                  <CardBody>
-                    <CustomInput
-                      labelText="First Name..."
-                      id="first"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "text",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <People className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        )
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Email..."
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "email",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Email className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        )
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Password"
-                      id="pass"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "password",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off"
-                      }}
-                    />
-                  </CardBody>
-                  <CardFooter className={classes.cardFooter}>
-                    <Button simple color="primary" size="lg">
-                      Get started
-                    </Button>
-                  </CardFooter>
-                </form>
-              </Card>
-            </GridItem>
-          </GridContainer>
+                    </CardFooter>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+          <Footer whiteFont />
         </div>
-        <Footer whiteFont />
       </div>
-    </div>
   );
 }
